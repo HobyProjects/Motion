a6a1545bfb6de972a5eb351639c70562838762ba
HobyProjects <hobbyprojects.dev@gmail.com>
2025-04-27 23:50:23 +0530

    Add CMake configuration and installation for ImGui docking library
    - Created CMakeLists.txt for the ImGui docking library, defining the library, its headers, and sources.
    - Set up include directories and linked necessary dependencies (GLFW and OpenGL).
    - Added installation rules for the library, headers, and CMake package configuration files.
    - Introduced pkg-config support with imgui.pc.in for easier integration.
    - Created imguiConfig.cmake.in for package configuration management.

---
+++ Src/Core/Base/Base.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstdint>
+#include <type_trait>
+
+#ifdef MOTION_DEBUG
+
+#if defined(MOTION_PLATFORM_WINDOWS)
+#define MOTION_DEBUGBREAK() __debugbreak()
+
+#elif defined(MOTION_PLATFORM_LINUX)
+
+#include <signal.h>
+#define MOTION_DEBUGBREAK() raise(SIGTRAP)
+
+#else
+#error "Platform doesn't support debugbreak yet!"
+#endif
+
+#define MOTION_INSTRUMENTS_ENABLED
+#define MOTION_ASSERTS_ENABLED
+
+#else
+
+#define MOTION_DEBUGBREAK()
+
+#endif
+
+template<uint32_t vShift>
+struct Bits
+{
+	static constexpr uint32_t value = 1 << vShift;
+	using type = uint32_t;
+};
+
+#define MTION_MACRO_STR(xVal) #xVal 

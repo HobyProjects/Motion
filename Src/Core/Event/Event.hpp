f4acf8924260a5f7d46eb384f24ad36e0c010a92
HobyProjects <hobbyprojects.dev@gmail.com>
2025-04-28 23:30:00 +0530

    Add event handling classes and update CMake configuration

---
+++ Src/Core/Event/Event.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <string_view>
+#include <functional>
+#include <type_traits>
+
+#include "Base.hpp"
+
+namespace Motion::Core
+{
+    enum class EventCategory : uint32_t 
+    {
+        Window = Bits<0>::value,
+        Keyboard = Bits<1>::value,
+        Mouse = Bits<2>::value,
+        GamePad = Bits<3>::value,
+        Unknown = Bits<4>::value
+    };
+
+    enum class EventType : uint32_t
+    {
+        WindowClose = Bits<0>::value,
+        WindowResize = Bits<1>::value,
+        WindowPosChange = Bits<2>::value,
+        WindowFocusGain = Bits<3>::value,
+        WindowFocusLost = Bits<4>::value,
+        WindowFrameBufferSizeChange = Bits<5>::value,
+        WindowMaximize = Bits<6>::value,
+        WindowMinimize = Bits<7>::value,
+
+        KeyboardKeyPress = Bits<8>::value,
+        KeyboardKeyRelease = Bits<9>::value,
+        KeyboardKeyRepeate = Bits<10>::value,
+        KeybaordKeyChar = Bits<11>::value,
+
+        MouseButtonDown = Bits<12>::value,
+        MouseButtonUp = Bits<13>::value,
+        MouseWheelScroll = Bits<14>::value,
+        MouseCursorPosChange = Bits<15>::value,
+        MouseCursorWindowEnter = Bits<16>::value,
+        MouseCursorWindowLeave = Bits<17>::value
+    };
+
+    #define EVENT_CLASS_TYPE(EVENT_TYPE) static EventType StaticType() { return EVENT_TYPE; }\
+        virtual EventType Type() const override { return StaticType(); }\
+        virtual std::string_view What() const override { return std::string_view(#EVENT_TYPE); }
+
+    #define EVENT_CLASS_CATEGORY(EVENT_CATEGORY) virtual EventCategory Category() const override { return EVENT_CATEGORY; }\
+        virtual bool Equals(EventCategory category) const override { return static_cast<bool>(Category() == category); }
+
+    class IEvent
+    {
+        public:
+            IEvent() = default;
+            virtual ~IEvent() = default;
+
+            virtual EventType Type() const = 0;
+            virtual std::string_view What() const = 0;
+            virtual EventCategory Category() const = 0;
+            virtual bool Equals(EventCategory category) const = 0;
+    };
+
+    class EventHandler
+    {
+        public:
+            EventHandler(uint32_t windowHandle, IEvent& eventRef):
+                m_Event(eventRef), m_WinHandle(windowHandle){}
+            ~EventHandler() = default;
+
+            template<typename WinHandle, typename TEvent>
+            bool Dispatch(const std::function<bool(WinHandle, TEvent)>& callBackFun)
+            {
+                static_assert(std::is_base_of_v<TEvent, IEvent>, "TEvent must be a base class of IEvent");
+                if(m_Event.Type() == TEvent::StaticType())
+                {
+                    m_IsEventHandled |= callBackFun(m_WinHandle, static_cast<TEvent&>(m_Event));
+                    return m_IsEventHandled;
+                }
+            }
+
+            bool IsHandled() const { return m_IsEventHandled; }
+
+        private:
+            IEvent& m_Event;
+            uint32_t m_WinHandle{0};
+            bool m_IsEventHandled{false};
+    };
+
+    #define EVENT_CALLBACK(CALLBACK_FUNC) [this](auto&&... args) -> decltype(auto) { return this->CALLBACK_FUNC(std::forward<decltype(args)>(args)...); }
+}

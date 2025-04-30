7dcbb8a7521d5c136dabd71e7a1a8cc0f19c67d0
HobyProjects <hobbyprojects.dev@gmail.com>
2025-04-30 12:53:12 +0530

    Add keyboard and mouse event classes with key and mouse button enums

---
+++ Src/Core/Event/EventKeyboard.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "Event.hpp"
+#include "KeyCodes.hpp"
+
+namespace Motion::Core
+{
+    template<typename T>
+    requires std::is_enum_v<T> && std::is_convertible_v<T, KeyCode>
+    class EventKeyboardKeyPress : public IEvent
+    {
+        public:
+            EventKeyboardKeyPress(T keyCode) : m_KeyCode(keyCode) {}
+            virtual ~EventKeyboardKeyPress() = default;
+
+            EVENT_CLASS_TYPE(EventType::KeyboardKeyPress);
+            EVENT_CLASS_CATEGORY(EventCategory::Keyboard);
+
+            T KeyCode() const { return m_KeyCode; }
+            KeyState State() const { return KEY_PRESSED;}
+
+        private:
+            T m_KeyCode{ static_cast<T>(KeyCode::Unknown) };
+    };
+
+    template<typename T>
+    requires std::is_enum_v<T> && std::is_convertible_v<T, KeyCode>
+    class EventKeyboardKeyRelease : public IEvent
+    {
+        public:
+            EventKeyboardKeyRelease(T keyCode) : m_KeyCode(keyCode) {}
+            virtual ~EventKeyboardKeyRelease() = default;
+
+            EVENT_CLASS_TYPE(EventType::KeyboardKeyRelease);
+            EVENT_CLASS_CATEGORY(EventCategory::Keyboard);
+
+            T KeyCode() const { return m_KeyCode; }
+            KeyState State() const { return KEY_RELEASED;}
+
+        private:
+            T m_KeyCode{ static_cast<T>(KeyCode::Unknown) };
+    };
+
+    template<typename T>
+    requires std::is_enum_v<T> && std::is_convertible_v<T, KeyCode>
+    class EventKeyboardKeyRepeate : public IEvent
+    {
+        public:
+            EventKeyboardKeyRepeate(T keyCode) : m_KeyCode(keyCode) {}
+            virtual ~EventKeyboardKeyRepeate() = default;
+
+            EVENT_CLASS_TYPE(EventType::KeyboardKeyRepeate);
+            EVENT_CLASS_CATEGORY(EventCategory::Keyboard);
+
+            T KeyCode() const { return m_KeyCode; }
+            KeyState State() const { return KEY_REPEATED;}
+
+        private:
+            T m_KeyCode{ static_cast<T>(KeyCode::Unknown) };
+    };
+
+    class EventKeyboardKeyChar : public IEvent
+    {
+        public:
+            EventKeyboardKeyChar(uint32_t character) : m_Character(character) {}
+            virtual ~EventKeyboardKeyChar() = default;
+
+            EVENT_CLASS_TYPE(EventType::KeybaordKeyChar);
+            EVENT_CLASS_CATEGORY(EventCategory::Keyboard);
+
+            uint32_t CodePoint() const { return m_Character; }
+            KeyState State() const { return KEY_PRESSED; }
+
+        private:
+            uint32_t m_Character{ 0 };
+    };
+
+}
---
+++ Src/Core/Event/EventMouse.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "Event.hpp"
+#include "KeyCodes.hpp"
+
+namespace Motion::Core
+{
+    template<typename T>
+    requires std::is_enum_v<T> && std::is_constructible_v<T, MouseButton>
+    class EventMouseButtonDown : public IEvent
+    {
+        public:
+            EventMouseButtonDown(T button) : m_Button(button) {}
+            virtual ~EventMouseButtonDown() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseButtonDown);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+
+            T Button() const { return m_Button; }
+            MouseButtonState State() const { return MOUSE_BUTTON_PRESSED; }
+
+        private:
+            T m_Button{ static_cast<T>(MOUSE_BUTTON_NONE) };
+    };
+
+    template<typename T>
+    requires std::is_enum_v<T> && std::is_constructible_v<T, MouseButton>
+    class EventMouseButtonUp : public IEvent
+    {
+        public:
+            EventMouseButtonUp(T button) : m_Button(button) {}
+            virtual ~EventMouseButtonUp() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseButtonUp);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+
+            T Button() const { return m_Button; }
+            MouseButtonState State() const { return MOUSE_BUTTON_RELEASED; }
+
+        private:
+            T m_Button{ static_cast<T>(MOUSE_BUTTON_NONE) };
+    };
+
+    template<typename T>
+    requires std::is_integral_v<T> || std::is_floating_point_v<T>
+    class EventMouseWheelScroll : public IEvent
+    {
+        public:
+            EventMouseWheelScroll(T offsetX, T offsetY) : m_OffsetX(offsetX), m_OffsetY(offsetY) {}
+            virtual ~EventMouseWheelScroll() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseWheelScroll);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+
+            T OffsetX() const { return m_OffsetX; }
+            T OffsetY() const { return m_OffsetY; }
+
+        private:
+            T m_OffsetX{}, m_OffsetY{};
+    };
+
+    template<typename T>
+    requires std::is_integral_v<T> || std::is_floating_point_v<T>
+    class EventMouseCursorMove : public IEvent
+    {
+        public:
+            EventMouseCursorMove(T x, T y) : m_X(x), m_Y(y) {}
+            virtual ~EventMouseCursorMove() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseCursorPosChange);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+
+            T GetX() const { return m_X; }
+            T GetY() const { return m_Y; }
+
+        private:
+            T m_X{}, m_Y{};
+    };
+
+    class EventMouseCursorWindowEnter : public IEvent
+    {
+        public:
+            EventMouseCursorWindowEnter() = default;
+            virtual ~EventMouseCursorWindowEnter() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseCursorWindowEnter);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+    };
+
+    class EventMouseCursorWindowLeave : public IEvent
+    {
+        public:
+            EventMouseCursorWindowLeave() = default;
+            virtual ~EventMouseCursorWindowLeave() = default;
+
+            EVENT_CLASS_TYPE(EventType::MouseCursorWindowLeave);
+            EVENT_CLASS_CATEGORY(EventCategory::Mouse);
+    };
+}
